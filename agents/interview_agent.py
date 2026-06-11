033358235b6e11c3afb3929fcc0998e98474b4f3
neerajkorrapati <neerajkorrapati36@gmail.com>
2026-06-11 13:43:21 +0530

    Day_10: Added Interview_Agent to input the context and identify important topics for preparation

---
+++ agents/interview_agent.py
@@ -0,0 +1,21 @@
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client=genai.Client()
+def interview_agent(context):
+    prompt="""
+    Analyze the retrieved Context :
+    {context}
+    And identify the most important topics for Preparation.
+    """
+    repsonse=client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt,
+        config={
+            "temperature":0.5,
+            "max_output_tokens":500,
+        }
+    )
+    return response.text
