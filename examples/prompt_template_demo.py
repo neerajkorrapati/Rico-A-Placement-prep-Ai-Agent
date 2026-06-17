825d1ae982e13592b3888b2b20631dce54ebb9e4
neerajkorrapati <neerajkorrapati36@gmail.com>
2026-06-17 12:45:04 +0530

    Day15: Simple langchain implementation using gemini

--- examples/prompt_template_demo.py
+++ examples/prompt_template_demo.py
@@ -1,15 +1,14 @@
 from langchain_core.prompts import PromptTemplate
-
-template= PromptTemplate.from_template(
-"""
-Analyze the following resume :
-
-{resume}
-"""
+template=PromptTemplate.from_template(
+    """
+    analyze the following resume :
+    
+    {resume}
+    """
 )
 prompt=template.invoke(
     {
-    "resume":"Python AI Projects" #here langchain replaces resume in the above prompt_template with Python ai projects
+        "resume":"Python ai projects"
     }
 )
-print(prompt.text)
+print (prompt.text)
