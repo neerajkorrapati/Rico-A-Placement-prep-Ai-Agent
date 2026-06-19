16e06d30c154949bfc4f87ed8d4496882bbf93a2
neerajkorrapati <neerajkorrapati36@gmail.com>
2026-06-19 11:41:32 +0530

    Day16: Testing multi - node langGraph

--- tests/test_simple_lang_graph.py
+++ tests/test_simple_lang_graph.py
@@ -1,4 +1,5 @@
-from typing import TypedDict    
+from typing import TypedDict   
+
 from langgraph.graph import(
     StateGraph,START,END
 )
@@ -9,11 +10,23 @@ def add_two(state:State):
     return{
         "number":state["number"]+2
     }
+
+def mul_3(state:State):
+    return{
+        "number":state["number"]*3
+    }
+
 graph =StateGraph(State)
+
 graph.add_node("add_two",add_two)
 
+graph.add_node("mul_3",mul_3)
+
+
 graph.add_edge(START,"add_two")
-graph.add_edge("add_two",END)
+graph.add_edge("add_two","mul_3")
+graph.add_edge("mul_3",END)
+
 workflow=graph.compile()
 
 result=workflow.invoke(
